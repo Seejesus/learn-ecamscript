bce1ce3244673ee8e03b4a55f2fd5fce890c04cc
Seejesus <kkt7444@gmail.com>
2022-03-05 22:11:35 +0900

    Scope

--- public/lecture/var-let-const.js
+++ public/lecture/var-let-const.js
@@ -20,6 +20,26 @@
 // console.log(TO_BE_JOB);
 
 
+// console.log("JavaScript 프로그래밍");
+
+// var variable = "global 변수";
+// const PI = 3.141592;
+
+// {
+//   let variable = "블럭의 지역 변수";
+//   console.log(variable);
+// }
+
+// console.log(variable);
+// console.log(`PI = ${PI}`);
+
+// scope = boundary (영역)
+// 변수의 유효범위
+
+// var 스코프 - 함수 내부 ..함수 안에서만 스코프를 갖는다
+// let, const를 사용할 경우 - 함수 + 블록으로 스코프를 갖는다
+
+
 console.log("JavaScript 프로그래밍");
 
 var variable = "global 변수";
@@ -29,6 +49,16 @@ const PI = 3.141592;
   let variable = "블럭의 지역 변수";
   console.log(variable);
 }
+// 문인데...블럭이 있는 if문 반복문...함수 선언문...은 블럭 구문은 세미클론이 안들어간다.
+// IF, Wwhie 문에는 세미콜이 안붇는다
+// 식과 문... 식에는 ..함수표현식에는 붙는다..
+
+function functionScope() {
+  //함수의 실행 영역
+  var variable = "functiond의 Local 변수";
+  console.log(variable);
+}
+functionScope();
 
 console.log(variable);
 console.log(`PI = ${PI}`);
